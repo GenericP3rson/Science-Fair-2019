a88c943c7f1aff39a9847a683b6e0b10b4a117ab
Paromita Datta <paromitadatta@Paromitas-MacBook-Pro.local>
2019-02-02 16:53:27 -0600

    It overfits a lot, but it is functional

--- python_stuff/harvard.py
+++ python_stuff/harvard.py
@@ -7,18 +7,49 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 import pandas as pd
 from PIL import Image
+import random
 
 # data, labels = load_csv('harvard/HAM10000_metadata.csv', target_column=2, categorical_labels=True, n_classes=2)
 # So the labels must be an integer. I can either edit the file or read my data in a different way?
 
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
-dataset = pd.read_csv("harvard/HAM10000_metadata.csv") # Opens the dataset
+
+
+def open_and_random(dataset):
+    '''
+    This will open a CSV, mix it, then turn it back into a DataFrame.
+    '''
+    dataset = pd.read_csv(dataset)  # Opens the dataset
+    dataset = [dataset.iloc[i] for i in range(len(dataset))]
+    random.shuffle(dataset)  # Shuffles the dataset
+    al = []
+    for row in range(len(dataset[0])):
+        mini = []
+        for i in dataset:
+            mini.append(i[row])
+        al.append(mini)
+    df = pd.DataFrame({"image_id": al[1]})
+    df["dx"] = al[2]
+    print(df)
+    return df
+
+
+dataset = open_and_random("harvard/HAM10000_metadata.csv")
 # print(stuff.loc[0]["image_id"])
 # Two ways to access: stuff.loc[][] or stuff.iloc[][]
 # print(np.asarray(stuff.loc[:, "image_id"]))
-limit = 25
-lim = 50
-image_list = list(dataset.loc[:, "image_id"])[0:limit] # Images in a list
+data_limit_low = 1075
+data_limit_mid1 = 1125
+data_limit_mid2 = 1125
+data_limit_hi = 1150
+data = list(dataset.loc[:, "image_id"])  # Images in a list
+image_list = data[data_limit_low:data_limit_mid1]
+test_images = data[data_limit_mid2:data_limit_hi]
+all_labels = list(dataset.loc[:, "dx"])
+labels = all_labels[data_limit_low:data_limit_mid1]
+test_labels = all_labels[data_limit_mid2:data_limit_hi]
+
+
 # image_array = np.asarray(dataset.loc[:, "image_id"]) # Images in a numpy array
 # These both have the data (I'm not sure how I what to store it)
 def convert(labels):
@@ -36,53 +67,68 @@ def convert(labels):
     6 vascular lesions (angiomas, angiokeratomas, pyogenic granulomas and hemorrhage, vasc).
     '''
     key = [
-        ["akiec", [1, 0, 0, 0, 0, 0, 0]], 
-        ["bcc", [0, 1, 0, 0, 0, 0, 0]], 
-        ["bkl", [0, 0, 1, 0, 0, 0, 0]], 
-        ["df", [0, 0, 0, 1, 0, 0, 0]], 
-        ["mel", [0, 0, 0, 0, 1, 0, 0]], 
-        ["nv", [0, 0, 0, 0, 0, 1, 0]], 
+        ["akiec", [1, 0, 0, 0, 0, 0, 0]],
+        ["bcc", [0, 1, 0, 0, 0, 0, 0]],
+        ["bkl", [0, 0, 1, 0, 0, 0, 0]],
+        ["df", [0, 0, 0, 1, 0, 0, 0]],
+        ["mel", [0, 0, 0, 0, 1, 0, 0]],
+        ["nv", [0, 0, 0, 0, 0, 1, 0]],
         ["vasc", [0, 0, 0, 0, 0, 0, 1]]
-        ]  # This is the key
+    ]  # This is the key
     # labels = list(dataset.loc[:, "dx"])
     # print(labels)
     index = 0
-    for lab in labels: # This is where the converting happens
+    for lab in labels:  # This is where the converting happens
         for disease in key:
             if lab == disease[0]:
                 labels[index] = disease[1]
                 break
-        index+=1
+        index += 1
     # print(labels)
-    labels = np.asarray(labels) # Changes it to array
-    labels.resize(len(labels), 7) # Makes it 2d
-    # print(labels)
+    labels = np.asarray(labels)  # Changes it to array
+    labels.resize(len(labels), 7)  # Makes it 2d
+    print(labels)
     return labels
-labels = convert(list(dataset.loc[:, "dx"])[0:limit]) # Inserts the dx column to be converted
-print(labels)
-test_labels = convert(list(dataset.loc[:, "dx"])[limit+1:lim])
+
+
+labels = convert(labels)  # Inserts the dx column to be converted
+# print(labels)
+test_labels = convert(test_labels)
 
 # So I have all the labels in the correct format, now all I have to do is load up the images and put them in the correct format
 
-# ALL IMAGES ARE (600, 450)
-fout = open("cancer_photos.txt", "w")
-all_pixels = []
-for image_name in image_list:
-    image = Image.open("harvard/full_set/" + image_name + ".jpg")
-    # print(str(image.size) + " = " + str(len(image.getdata())) + " total pixels.")
-    # print(image.convert("RGB"))
-    # print(list(image.getdata()))
-    RGBvalues = list(i[0:3] for i in image.getdata())  # We have a list of the rgb values
-    # print(RGBvalues)
-    # print(np.asarray(RGBvalues))
-    fout.write(' '.join(' '.join(str(j) for j in i) for i in RGBvalues))
-    fout.write("\n")
-    all_pixels.append(np.asarray(RGBvalues))
-    # x = image.size[0]
-    # y = image.size[1]
-all_pixels = np.asarray(all_pixels)
-all_pixels.resize(len(image_list), 600, 450, 3)
-print(all_pixels)
+
+def convert_data(image_list):
+    '''
+    This converts the image names (in a list) to an array of pixels.
+    This also writes everything to "cancer_photos.txt" 'cause why not?
+    '''
+    # ALL IMAGES ARE (600, 450)
+    # fout = open("cancer_photos.txt", "w")
+    all_pixels = []
+    for image_name in image_list:
+        image = Image.open("harvard/full_set/" + image_name + ".jpg")
+        # print(str(image.size) + " = " + str(len(image.getdata())) + " total pixels.")
+        # print(image.convert("RGB"))
+        # print(list(image.getdata()))
+        # We have a list of the rgb values
+        RGBvalues = list(i[0:3] for i in image.getdata())
+        # print(RGBvalues)
+        # print(np.asarray(RGBvalues))
+        # fout.write(' '.join(' '.join(str(j) for j in i) for i in RGBvalues))
+        # fout.write("\n")
+        all_pixels.append(np.asarray(RGBvalues))
+        x = image.size[0]
+        y = image.size[1]
+    all_pixels = np.asarray(all_pixels)
+    all_pixels.resize(len(image_list), x, y, 3)
+    print(all_pixels)
+    return all_pixels
+
+
+all_pixels = convert_data(image_list)
+test_pixel = convert_data(test_images)
+
 
 all_pixels = all_pixels.reshape([-1, 600, 450, 3])
 
@@ -95,21 +141,23 @@ net = conv_2d(net, 200, 2, activation='relu')
 net = max_pool_2d(net, 10)
 
 net = fully_connected(net, 1000, activation='relu')
-net = dropout(net, 0.65)
+net = dropout(net, 0.5)
 
 net = fully_connected(net, 7, activation='softmax')
-net = regression(net, optimizer='adam', learning_rate=0.01,
-                     loss='categorical_crossentropy', name='targets')
+net = regression(net, optimizer='adam', learning_rate=0.001,
+                 loss='categorical_crossentropy', name='targets')
 
 model = tflearn.DNN(net)
 
 # Run this to train the data
-model.fit({'input': all_pixels}, {'targets': labels}, n_epoch=10, 
+model.fit({'input': all_pixels}, {'targets': labels}, n_epoch=10, validation_set=({'input': test_pixel}, {'targets': test_labels}),
           snapshot_step=500, show_metric=True, run_id='cancer')
 
 model.save("cancer.model")
 
+'''
 # Run this to check the data
-# model.load('tfl.model')
-# print(np.round(model.predict([test_x[10]])[0]))
-# print(test_y[10])
+model.load('cancer.model')
+print((model.predict([test_pixel[7]])[0]))
+print(test_labels[7])
+'''
